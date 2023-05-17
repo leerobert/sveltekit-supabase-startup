5ab4efb1d411c1098f21acd8e5bd9e701a019979
Robert Lee <lee.robert.138@gmail.com>
2023-05-17 09:48:45 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<!-- src/routes/+layout.svelte -->
+<script lang="ts">
+	import '../app.postcss';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
+</script>
+
+<svelte:head>
+	<title>deff</title>
+</svelte:head>
+
+<slot />
